07a685a2cfc02423c713c61a058d2b5db68e7068
Sam Deane <sam@elegantchaos.com>
2022-02-21 21:44:16 +0000

    added int tests

--- Tests/BinaryCodingTests/BinaryCodingTests.swift
+++ Tests/BinaryCodingTests/BinaryCodingTests.swift
@@ -9,10 +9,68 @@ import XCTestExtensions
 @testable import BinaryCoding
 
 final class BinaryCodingTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(BinaryCoding().text, "Hello, World!")
+    func testInt() throws {
+        
+        let encoder = BinaryEncoder()
+        
+        let expected = Data([0xF0, 0xDE, 0xBC, 0x9A, 0x78, 0x56, 0x34, 0x12])
+        XCTAssertEqual(try encoder.encode(Int(0x123456789ABCDEF0)), expected)
     }
+
+    func testInt32() throws {
+        
+        let encoder = BinaryEncoder()
+        
+        let expected = Data([0x78, 0x56, 0x34, 0x12])
+        XCTAssertEqual(try encoder.encode(Int32(0x12345678)), expected)
+    }
+
+    func testInt16() throws {
+        
+        let encoder = BinaryEncoder()
+        
+        let expected = Data([0x34, 0x12])
+        XCTAssertEqual(try encoder.encode(Int16(0x1234)), expected)
+    }
+
+    func testInt8() throws {
+        
+        let encoder = BinaryEncoder()
+        
+        let expected = Data([0x12])
+        XCTAssertEqual(try encoder.encode(Int8(0x12)), expected)
+    }
+
+    func testUInt() throws {
+        
+        let encoder = BinaryEncoder()
+        
+        let expected = Data([0xF0, 0xDE, 0xBC, 0x9A, 0x78, 0x56, 0x34, 0x12])
+        XCTAssertEqual(try encoder.encode(UInt(0x123456789ABCDEF0)), expected)
+    }
+
+    func testUInt32() throws {
+        
+        let encoder = BinaryEncoder()
+        
+        let expected = Data([0x78, 0x56, 0x34, 0x12])
+        XCTAssertEqual(try encoder.encode(UInt32(0x12345678)), expected)
+    }
+
+    func testUInt16() throws {
+        
+        let encoder = BinaryEncoder()
+        
+        let expected = Data([0x34, 0x12])
+        XCTAssertEqual(try encoder.encode(UInt16(0x1234)), expected)
+    }
+
+    func testUInt8() throws {
+        
+        let encoder = BinaryEncoder()
+        
+        let expected = Data([0x12])
+        XCTAssertEqual(try encoder.encode(UInt8(0x12)), expected)
+    }
+
 }
