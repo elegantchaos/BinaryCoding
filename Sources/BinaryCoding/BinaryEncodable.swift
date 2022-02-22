77feb4bd2587aea4bfdb4b5b720d2129dabe887e
Sam Deane <sam@elegantchaos.com>
2022-02-22 18:51:32 +0000

    Added fallback binaryEncode for normal Encodables

--- Sources/BinaryCoding/BinaryEncodable.swift
+++ Sources/BinaryCoding/BinaryEncodable.swift
@@ -15,7 +15,26 @@ public protocol BinaryEncodable: Encodable {
 }
 
 public extension BinaryEncodable {
+    /// Binary encode this item by falling back to the normal
+    /// encode method.
+    ///
+    /// Types that want to provide different encoding behaviour
+    /// for binary coders should provide their own implementation
+    /// of this method.
     func binaryEncode(to encoder: BinaryEncoder) throws {
         try encode(to: encoder)
+    }
+}
+
+public extension Encodable {
+    /// Binary encode this item if possible, otherwise encode it normally.
+    /// Performs a dynamic check to see if the item and encoder support
+    /// binary encoding. If not, calls the normal encode method.
+    func binaryEncode(to encoder: Encoder) throws {
+        if let bc = self as? BinaryCodable, let be = encoder as? BinaryEncoder {
+            try bc.binaryEncode(to: be)
+        } else {
+            try encode(to: encoder)
+        }
     }
 }
