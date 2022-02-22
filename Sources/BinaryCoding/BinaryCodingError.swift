e28c91214115d63e5b052dcf82595f35fb2c4079
Sam Deane <sam@elegantchaos.com>
2022-02-22 16:14:20 +0000

    Extra bits from original project

---
+++ Sources/BinaryCoding/BinaryCodingError.swift
@@ -0,0 +1,12 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 22/02/22.
+//  All code (c) 2022 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import Foundation
+
+public enum BinaryCodingError: Error {
+    case outOfData
+    case badStringEncoding
+}
+
